4592954d3ebefd71d80b5da7dc0bfc6a8280f51a
Srikanth Hebbar <Srikanth.hebbar14@outlook.com>
2021-10-07 21:24:58 +0530

    Till frontend authentication

--- client/pages/auth/signin.js
+++ client/pages/auth/signin.js
@@ -43,7 +43,7 @@ const Signup = () => {
           />
         </div>
         <div className="form-group" style={{ marginTop: '2%' }}>
-          <button className="btn btn-primary">Signin</button>
+          <button className="btn btn-primary">Sign in</button>
         </div>
       </form>
     </div>
